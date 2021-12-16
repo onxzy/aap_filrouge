5709c55e6554bd3a292b7617c970be73cbe6d561
Céline <celine.barbet@centrale.centralelille.fr>
2021-12-16 09:55:09 +0100

    test balance

--- main.c
+++ main.c
@@ -25,17 +25,44 @@ int main(int argc, char ** argv) {
 	outputPath = "output";
 	
 	// 8, 11, 16, 5, 7, 14, 6, 1, 3, 10, 12,16
+	// insertAVL(&root, 8);
+	// insertAVL(&root, 11);
+	// insertAVL(&root, 16);
+	// insertAVL(&root, 5);
+	// insertAVL(&root, 7);
+	// insertAVL(&root, 14);
+	// insertAVL(&root, 6);
+	// insertAVL(&root, 1);
+	// insertAVL(&root, 3);
+	// insertAVL(&root, 10);
+	// insertAVL(&root, 12);
+
 	insertAVL(&root, 8);
-	insertAVL(&root, 11);
-	insertAVL(&root, 16);
-	insertAVL(&root, 5);
-	insertAVL(&root, 7);
-	insertAVL(&root, 14);
-	insertAVL(&root, 6);
 	insertAVL(&root, 1);
+	insertAVL(&root, 7);
+	insertAVL(&root, 4);
+	insertAVL(&root, 9);
+	insertAVL(&root, 2);
 	insertAVL(&root, 3);
+	insertAVL(&root, 17);
+	insertAVL(&root, 23);
 	insertAVL(&root, 10);
+	insertAVL(&root, 16);
+	insertAVL(&root, 24);
+	insertAVL(&root, 11);
+	insertAVL(&root, 6);
+	insertAVL(&root, 15);
+	insertAVL(&root, 22);
+	insertAVL(&root, 13);
 	insertAVL(&root, 12);
+	insertAVL(&root, 5);
+	insertAVL(&root, 21);
+	insertAVL(&root, 25);
+	insertAVL(&root, 20);
+	insertAVL(&root, 19);
+	insertAVL(&root, 18);
+	insertAVL(&root, 14);
+	
 
 	// printf("\n\n#1\n");
 	// insertAVL(&root, 8);
